163cfa14a0c0dfe59a033748dee9393821388a46
OdilovMM <133968972+OdilovMM@users.noreply.github.com>
2023-06-11 10:31:58 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,25 @@
+const formCreate = document.getElementById("form-create");
+const formEdit = document.getElementById("form-edit");
+const listGroupTodo = document.getElementById("list-group-todo");
+// const messageCreate = document.getElementById('message-create')
+const time = document.getElementById("time");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+/* time elements */
+const fullDay = document.getElementById("full-day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const closeEl = document.getElementById("close");
+
+//check local storage
+
+let todos = JSON.parse(localStorage.getItem("list"))
+  ? JSON.parse(localStorage.getItem("list"))
+  : [];
+
+//get todos
+
+formCreate.addEventListener("submit", (e) => {
+  e.preventDefault;
+});
